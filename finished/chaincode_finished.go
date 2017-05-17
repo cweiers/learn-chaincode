c693c5bb1beddf2c12d69e315bb66b2c76ba89dd
Christian Weiers <christian.weiers@materna.de>
2017-05-17 15:14:44 +0200

    typo

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -48,7 +48,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	return nil, nil
 }
 
-// Invoke isur entry point to invoke a chaincode function
+// Invoke is your entry point to invoke a chaincode function
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
