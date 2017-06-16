20e0d73149f344fae8f5906949675db7c504c3f3
Christian <christian.weiers@rub.de>
2017-06-16 18:42:26 +0200

    add SLA

--- prototype/Escalator.go
+++ prototype/Escalator.go
@@ -12,9 +12,18 @@ import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
-const ()
-
 type SimpleChaincode struct {
+}
+
+//simple SLA.
+type ServiceLevelAgreement struct {
+	ServiceProvider string
+	TimeToArrive    int64 // contractually agreed time for a mechanic to arrive
+	TimeToRepair    int64 // contractually agreed time for the repairs to be finished
+	// representing the number of times the agreement was fulfilled, slightly violated or severely violated
+	None   int //no violation
+	Light  int
+	Severe int
 }
 
 type Escalator struct {
@@ -38,9 +47,9 @@ type Ticket struct {
 	SpEmployee      string // mechanic assigned by ServiceProvider
 	SpeCommentary   string // additional commentary, optionally to be filled out by the SpEmployee
 	EstRepairTime   string
-	TimeOfArrival   int64
+	TimeOfArrival   int64 // time of arrival
 	RepairStatus    string
-	FinalRepairTime int64
+	FinalRepairTime int64 // closing the ticket
 	FinalReport     string
 }
 
@@ -53,35 +62,32 @@ func main() {
 
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 
+	//initialise counters for ticket and escalator ID creation
 	stub.PutState("escalatorCounter", []byte("0"))
 	stub.PutState("ticketCounter", []byte("0"))
-	createDefaultEscalator(stub)
 
-	return nil, nil
-}
-
-func createDefaultEscalator(stub shim.ChaincodeStubInterface) error {
+	//create an escalator to use with createDefaultTicket
 	idAsString, _ := createID(stub, "escalator")
-	idAsString = "KI" + idAsString //Id is now the first two characters of the location + a sequential ID
+	idAsString = "DO" + idAsString
 	var escalator = Escalator{
 		EscalatorID:  idAsString,
-		Trainstation: "Kiel Hbf",
+		Trainstation: "Dortmung Hbf",
 		Platform:     "Gleis 4",
 	}
 
 	state, err := json.Marshal(escalator)
 
 	stub.PutState(idAsString, state)
-	if err != nil {
-		return err
-	}
-	return nil
+
+	return nil, nil
 }
 
 //Invoke is the entry point for all other asset altering functions called by an CC invocation
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 
 	switch function {
+	case "createSLA":
+		return t.createSLA(stub, args)
 	case "createEscalator":
 		return t.createEscalator(stub, args)
 	case "createTicket":
@@ -136,17 +142,59 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	return nil, errors.New("Received unknown function query")
 }
 
+// create a service level agreement for a given ServiceProvider. Input should be the ServiceProvider, the time in seconds from ticket creation
+// until arrival of a mechanic, and the time in seconds from ticket creation until the escalator repair is done.
+func (t *SimpleChaincode) createSLA(stub shim.ChaincodeStubInterface, args []string) error {
+
+	var sla ServiceLevelAgreement
+	sla.ServiceProvider = args[0]
+	sla.timeToArrive = args[1]
+	sla.timeToRepair = args[2]
+	sla.None = 0
+	sla.Light = 0
+	sla.Severe = 0
+	slaKey := "sla" + strings.ToLower(args[0])
+
+	slaAsByteArr, err := json.Marshal(sla)
+
+	if err != nil {
+		return err
+	}
+	stub.PutState(slaKey, slaAsByteArr)
+	return nil
+}
+
+//update a SLA from the world state with new values. A value for both timeToArrive and timeToRepair has to be supplied.
+func (t *SimpleChaincode) updateSLA(stub shim.ChaincodeStubInterface, args []string) error {
+	if args.len != 3 {
+		return errors.New("Needs name of the service provider, timeToArrive and timeToRepair. If one of the latter two does not change, the old value still has to be supplied")
+	}
+
+	var sla ServiceLevelAgreement
+	slaAsByteArr, err := stub.GetState("sla" + strings.ToLower(args[0]))
+	if err != nil {
+		return err
+	}
+	json.Unmarshal(slaAsByteArr, &sla)
+	sla.timeToArrive = args[1]
+	sla.timeToRepair = args[2]
+
+	slaAsByteArr, _ = json.Marshal(sla)
+	stub.PutState("sla"+strings.ToLower(args[0]), slaAsByteArr)
+	return nil
+}
+
 // Create a new ticket and store it on the ledger with TicketID as key.
 //
 func (t *SimpleChaincode) createTicket(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	if len(args) != 7 {
-		return nil, errors.New("Wrong number of arguments, must be 6: Timestamp, Trainstation, Platform, Device, TechPart, ErrorID and ErrorMessage")
+		return nil, errors.New("Wrong number of arguments, must be 6: Trainstation, Platform, Device, TechPart, ErrorID and ErrorMessage")
 	}
 	idAsString, _ := createID(stub, "ticket")
-	timeString := getTransactionTime(stub)
+	time := getTransactionTime(stub)
 	var ticket = Ticket{
 		TicketID:     idAsString,
-		Timestamp:    timeString,
+		Timestamp:    time,
 		Trainstation: args[0],
 		Platform:     args[1],
 		Device:       args[2],
@@ -175,11 +223,11 @@ func (t *SimpleChaincode) createDefaultTicket(stub shim.ChaincodeStubInterface, 
 	json.Unmarshal(defaultEscAsByteArr, &defaultEsc)
 
 	idAsString, _ := createID(stub, "ticket")
-	timeString := getTransactionTime(stub)
+	time := getTransactionTime(stub)
 
 	var ticket = Ticket{
 		TicketID:     idAsString,
-		Timestamp:    timeString,
+		Timestamp:    time,
 		Trainstation: defaultEsc.Trainstation,
 		Platform:     defaultEsc.Platform,
 		Device:       defaultEsc.EscalatorID,
@@ -304,9 +352,9 @@ func (t *SimpleChaincode) getTicketsByServiceProvider(stub shim.ChaincodeStubInt
 			if bArrayMemberAlreadyWritten == true {
 				buffer.WriteString(",")
 			}
-			buffer.WriteString("{")
+			//			buffer.WriteString("{")
 			buffer.WriteString(string(queryResultValue))
-			buffer.WriteString("}")
+			//			buffer.WriteString("}")
 			bArrayMemberAlreadyWritten = true
 		}
 
@@ -354,9 +402,9 @@ func (t *SimpleChaincode) getTicketsByStatus(stub shim.ChaincodeStubInterface, a
 				if bArrayMemberAlreadyWritten == true {
 					buffer.WriteString(",")
 				}
-				buffer.WriteString("{")
+				//				buffer.WriteString("{")
 				buffer.WriteString(string(queryResultValue))
-				buffer.WriteString("}")
+				//				buffer.WriteString("}")
 				bArrayMemberAlreadyWritten = true
 			}
 		}
@@ -378,9 +426,9 @@ func (t *SimpleChaincode) getTicketsByStatus(stub shim.ChaincodeStubInterface, a
 				if bArrayMemberAlreadyWritten == true {
 					buffer.WriteString(",")
 				}
-				buffer.WriteString("{")
+				//				buffer.WriteString("{")
 				buffer.WriteString(string(queryResultValue))
-				buffer.WriteString("}")
+				//				buffer.WriteString("}")
 				bArrayMemberAlreadyWritten = true
 			}
 		}
@@ -430,9 +478,9 @@ func (t *SimpleChaincode) getTicketsByMechanic(stub shim.ChaincodeStubInterface,
 			if bArrayMemberAlreadyWritten == true {
 				buffer.WriteString(",")
 			}
-			buffer.WriteString("{")
+			//			buffer.WriteString("{")
 			buffer.WriteString(string(queryResultValue))
-			buffer.WriteString("}")
+			//			buffer.WriteString("}")
 			bArrayMemberAlreadyWritten = true
 		}
 
@@ -479,9 +527,9 @@ func (t *SimpleChaincode) getWIPTickets(stub shim.ChaincodeStubInterface, args [
 			if bArrayMemberAlreadyWritten == true {
 				buffer.WriteString(",")
 			}
-			buffer.WriteString("{")
+			//			buffer.WriteString("{")
 			buffer.WriteString(string(queryResultValue))
-			buffer.WriteString("}")
+			//			buffer.WriteString("}")
 			bArrayMemberAlreadyWritten = true
 		}
 
@@ -524,9 +572,9 @@ func (t *SimpleChaincode) getNewSPTickets(stub shim.ChaincodeStubInterface, args
 			if bArrayMemberAlreadyWritten == true {
 				buffer.WriteString(",")
 			}
-			buffer.WriteString("{")
+			//			buffer.WriteString("{")
 			buffer.WriteString(string(queryResultValue))
-			buffer.WriteString("}")
+			//			buffer.WriteString("}")
 			bArrayMemberAlreadyWritten = true
 		}
 
@@ -572,9 +620,9 @@ func (t *SimpleChaincode) getAssignedSPTickets(stub shim.ChaincodeStubInterface,
 			if bArrayMemberAlreadyWritten == true {
 				buffer.WriteString(",")
 			}
-			buffer.WriteString("{")
+			//			buffer.WriteString("{")
 			buffer.WriteString(string(queryResultValue))
-			buffer.WriteString("}")
+			//			buffer.WriteString("}")
 			bArrayMemberAlreadyWritten = true
 		}
 
@@ -612,15 +660,9 @@ func (t *SimpleChaincode) getTicketsByRange(stub shim.ChaincodeStubInterface, ar
 			buffer.WriteString(",")
 		}
 
-		buffer.WriteString("{")
-		//		buffer.WriteString("\"")
-		//		buffer.WriteString(queryResultKey)
-		//		buffer.WriteString("\"")
-
-		//		buffer.WriteString(", \"Ticket\":")
-
+		//		buffer.WriteString("{")
 		buffer.WriteString(string(queryResultValue))
-		buffer.WriteString("}")
+		//		buffer.WriteString("}")
 		bArrayMemberAlreadyWritten = true
 	}
 	buffer.WriteString("]")
@@ -745,10 +787,7 @@ func (t *SimpleChaincode) finishRepair(stub shim.ChaincodeStubInterface, args []
 		return nil, errors.New("Wrong number of arguments, must be 1: TicketID")
 	}
 
-	var state []byte
-	var err error
-
-	state, err = stub.GetState(args[0])
+	state, err := stub.GetState(args[0])
 	if err != nil {
 		return nil, err
 	}
@@ -764,6 +803,24 @@ func (t *SimpleChaincode) finishRepair(stub shim.ChaincodeStubInterface, args []
 	}
 	stub.PutState(args[0], state) //write updated ticket to world state again
 
+	//update SLA depending on timestamps
+	var sla ServiceLevelAgreement
+	slaAsByteArr, _ := stub.GetState("sla" + strings.ToLower(ticket.ServiceProvider))
+	json.Unmarshal(slaAsByteArr, &sla)
+
+	ttA := ticket.TimeOfArrival - ticket.Timestamp   //time to arrive
+	ttR := ticket.FinalRepairTime - ticket.Timestamp //time to repair
+	switch {
+	case (tta < sla.TimeToArrive) && (ttR < sla.TimeToRepair): //All good
+		sla.None += 1
+	case (tta > sla.TimeToArrive+10800) || (ttR > sla.TimeToRepair+14400): //mechanic arrived more than 10800s = 3hours late OR it took more than 4 hours longer to repair overall
+		sla.Severe += 1
+	default:
+		sla.Light += 1
+	}
+
+	slaAsByteArr, _ = json.Marshal(sla)
+	stub.PutState("sla"+strings.ToLower(ticket.ServiceProvider), slaAsByteArr)
 	return nil, nil
 }
 
@@ -795,10 +852,7 @@ func (t *SimpleChaincode) writeFinalReport(stub shim.ChaincodeStubInterface, arg
 func getTransactionTime(stub shim.ChaincodeStubInterface) int64 {
 	timePointer, _ := stub.GetTxTimestamp()
 	return timePointer.Seconds
-	//	var t2 time.Time
-	//	t2 = time.Unix(timePointer.Seconds, 0) // set nanos as zero as we don`t need to display at this accuracy anyway
 
-	//	return t2
 }
 
 func leftPad2Len(s string, padStr string, overallLen int) string {
